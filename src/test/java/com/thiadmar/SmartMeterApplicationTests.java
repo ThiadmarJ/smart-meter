87619531857ebd1ea460e83c0f20fc02ff58ea76
Thiadmar <revoreverse@gmail.com>
2022-06-29 18:17:31 +0100

    spring boot initialised

---
+++ src/test/java/com/thiadmar/SmartMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiadmar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
